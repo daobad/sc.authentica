6ba3456467ab3e6d37e9a140e2f1b47bd3781b22
mahonggang <1584157651@qq.com>
2019-07-11 22:57:55 +0800

    first commit

---
+++ src/main/java/com/sc/authentica/SCAuthenticaApplication.java
@@ -0,0 +1,13 @@
+package com.sc.authentica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SCAuthenticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SCAuthenticaApplication.class, args);
+	}
+
+}
